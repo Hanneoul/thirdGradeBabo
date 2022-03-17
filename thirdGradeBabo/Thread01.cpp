39ab9c019201f664cbc02b45ec7b453ba7e2c1cd
PC2010221\YUHAN <e>
2022-03-17 12:56:05 +0900

    .

--- thirdGradeBabo/Thread01.cpp
+++ thirdGradeBabo/Thread01.cpp
@@ -1,43 +1,43 @@
-﻿#include <iostream>
-#include <thread>
-#include <chrono>
-
-using namespace std;
-
-int trigger;
-
-void seokHoon()
-{
-	while(trigger == 0)
-	{
-		cout << "나석훈 : 정석아 사랑해\n";
-		this_thread::sleep_for(std::chrono::milliseconds(1000));
-	}	
-}
-
-void JeongSeok()
-{
-	while (trigger == 0)
-	{
-		this_thread::sleep_for(std::chrono::milliseconds(500));
-		cout << "오정석 : 석훈아 사랑해\n";
-		this_thread::sleep_for(std::chrono::milliseconds(500));
-	}
-}
-
-int main()
-{
-	trigger = 0;
-	cout << "\n--그들의 사랑이 시작된다...\n\n";
-	thread na(seokHoon);
-	thread oh(JeongSeok);
-	
-	while (trigger==0)
-	{
-		cin >> trigger;
-	}
-
-	na.join();
-	oh.join();
-	cout << "\n--그들의 사랑이 끝났다...\n\n";
-}
+﻿//#include <iostream>
+//#include <thread>
+//#include <chrono>
+//
+//using namespace std;
+//
+//int trigger;
+//
+//void seokHoon()
+//{
+//	while(trigger == 0)
+//	{
+//		cout << "나석훈 : 정석아 사랑해\n";
+//		this_thread::sleep_for(std::chrono::milliseconds(1000));
+//	}	
+//}
+//
+//void JeongSeok()
+//{
+//	while (trigger == 0)
+//	{
+//		this_thread::sleep_for(std::chrono::milliseconds(500));
+//		cout << "오정석 : 석훈아 사랑해\n";
+//		this_thread::sleep_for(std::chrono::milliseconds(500));
+//	}
+//}
+//
+//int main()
+//{
+//	trigger = 0;
+//	cout << "\n--그들의 사랑이 시작된다...\n\n";
+//	thread na(seokHoon);
+//	thread oh(JeongSeok);
+//	
+//	while (trigger==0)
+//	{
+//		cin >> trigger;
+//	}
+//
+//	na.join();
+//	oh.join();
+//	cout << "\n--그들의 사랑이 끝났다...\n\n";
+//}
