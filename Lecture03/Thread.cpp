3ef6c8abe373473a5731d683c40a0f90f208d9c4
DESKTOP-CLMUJNV\elfle <elflee77@gmail.com>
2022-04-07 05:32:36 +0900

    [ES] 최종 정리

--- Lecture03/Thread.cpp
+++ Lecture03/Thread.cpp
@@ -43,36 +43,3 @@ int main()
 	oh.join();
 	cout << "\n--±×µéÀÇ »ç¶ûÀÌ ³¡³µ´Ù...\n\n";
 }
-
-#include <iostream>
-#include <map>
-#include <string>
-#include <chrono>
-#include <thread>
-#include <mutex>
-
-std::map<std::string, std::string> g_pages;
-std::mutex g_pages_mutex;
-
-void save_page(const std::string& url)
-{
-    std::this_thread::sleep_for(std::chrono::seconds(2));
-    std::string result = "fake content";
-
-    //std::lock_guard<std::mutex> guard(g_pages_mutex);
-    g_pages_mutex.lock();
-    g_pages[url] = result;
-    g_pages_mutex.unlock();
-}
-
-int main()
-{
-    std::thread t1(save_page, "http://foo");
-    std::thread t2(save_page, "http://bar");
-    t1.join();
-    t2.join();
-
-    for (const auto& pair : g_pages) {
-        std::cout << pair.first << " => " << pair.second << '\n';
-    }
-}
