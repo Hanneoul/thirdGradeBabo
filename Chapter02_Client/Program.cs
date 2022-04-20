b6c9642a6a621712d2a825ca127698d0ed45cd71
DESKTOP-CLMUJNV\elfle <elflee77@gmail.com>
2022-04-21 00:20:09 +0900

    .

--- Chapter02_Client/Program.cs
+++ Chapter02_Client/Program.cs
@@ -1,13 +1,8 @@
 ﻿using System.Net.Sockets;
 
 
-
-
-
 public class MyTcpClient
-{
-    
-
+{    
     public static void Main()
     {
         try
@@ -16,18 +11,12 @@ public class MyTcpClient
             string message = "Die!!";
 
             // Create a TcpClient.
-            // Note, for this client to work you need to have a TcpServer
-            // connected to the same address as specified by the server, port
-            // combination.%
+            
             Int32 port = 13000;
             TcpClient client = new TcpClient(server, port);
 
-            // Translate the passed message into ASCII and store it as a Byte array.
             Byte[] data = System.Text.Encoding.Default.GetBytes(message);
-
-            // Get a client stream for reading and writing.
-            //  Stream stream = client.GetStream();
-
+            
             NetworkStream stream = client.GetStream();
 
             // Send the message to the connected TcpServer.
