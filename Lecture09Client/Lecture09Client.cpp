590413bad5885b6ef8dcdbc507dd1a5597bda3fd
DESKTOP-CLMUJNV\elfle <elflee77@gmail.com>
2022-04-28 09:36:23 +0900

    .

--- Lecture09Client/Lecture09Client.cpp
+++ Lecture09Client/Lecture09Client.cpp
@@ -1,11 +1,18 @@
 ﻿#define WIN32_LEAN_AND_MEAN
 
+#include <queue>
+#include <conio.h>
+
 #include <windows.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <thread>
+#include <string>
+#include <iostream>
 
+using namespace std;
 
 // Need to link with Ws2_32.lib, Mswsock.lib, and Advapi32.lib
 #pragma comment (lib, "Ws2_32.lib")
@@ -14,21 +21,49 @@
 
 
 #define DEFAULT_BUFLEN 512
-#define DEFAULT_PORT "27015"
+#define DEFAULT_PORT "80"
+
+WSADATA wsaData;
+SOCKET ConnectSocket = INVALID_SOCKET;
+struct addrinfo* result = NULL,
+    * ptr = NULL,
+    hints;
+const char* sendbuf = "this is a Soo Client test";
+char recvbuf[DEFAULT_BUFLEN];
+int iResult;
+int recvbuflen = DEFAULT_BUFLEN;
+
+
+int Init();
+int Input();
+int Update();
+int Render();
+int Release();
+
 
 int main()
 {
-    WSADATA wsaData;
-    SOCKET ConnectSocket = INVALID_SOCKET;
-    struct addrinfo* result = NULL,
-        * ptr = NULL,
-        hints;
-    const char* sendbuf = "this is a Soo Client test";
-    char recvbuf[DEFAULT_BUFLEN];
-    int iResult;
-    int recvbuflen = DEFAULT_BUFLEN;
 
 
+   
+
+    if (!Init()) return 1;
+
+    bool isClosed = false;
+    while (isClosed)
+    {
+        if (!Input()) return 1;
+        if (!Update()) return 1;
+        if (!Render()) return 1;        
+    }
+
+    Release();
+
+    return 0;
+}
+
+int Init()
+{
     // Initialize Winsock
     iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
     if (iResult != 0) {
@@ -41,6 +76,8 @@ int main()
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_protocol = IPPROTO_TCP;
 
+
+
     // Resolve the server address and port
     iResult = getaddrinfo("127.0.0.1", DEFAULT_PORT, &hints, &result);
     if (iResult != 0) {
@@ -48,7 +85,55 @@ int main()
         WSACleanup();
         return 1;
     }
+    return 0;
+}
 
+int Input()
+{
+    if (_kbhit())
+    {        //키보드 입력 확인 (true / false)
+        char c = _getch();
+
+        string input;
+
+        if (c == ':')
+        {
+            //cin>>
+            if()
+        }
+    }
+}
+int Update()
+{
+
+}
+int Render()
+{
+
+}
+
+void Release()
+{
+    // cleanup
+    closesocket(ConnectSocket);
+    WSACleanup();
+}
+
+
+
+
+int InitializeWSA()
+{
+    
+
+    
+}
+
+
+
+
+int task_connect(string ip_port)
+{
     // Attempt to connect to an address until one succeeds
     for (ptr = result; ptr != NULL; ptr = ptr->ai_next) {
 
@@ -79,8 +164,13 @@ int main()
         return 1;
     }
 
+    return 0;
+}
+
+int task_send(const char *msg)
+{
     // Send an initial buffer
-    iResult = send(ConnectSocket, sendbuf, (int)strlen(sendbuf), 0);
+    iResult = send(ConnectSocket, msg, (int)strlen(msg), 0);
     if (iResult == SOCKET_ERROR) {
         printf("send failed with error: %d\n", WSAGetLastError());
         closesocket(ConnectSocket);
@@ -89,7 +179,10 @@ int main()
     }
 
     printf("Bytes Sent: %ld\n", iResult);
+}
 
+int shutdown_socket()
+{
     // shutdown the connection since no more data will be sent
     iResult = shutdown(ConnectSocket, SD_SEND);
     if (iResult == SOCKET_ERROR) {
@@ -111,10 +204,5 @@ int main()
             printf("recv failed with error: %d\n", WSAGetLastError());
 
     } while (iResult > 0);
-
-    // cleanup
-    closesocket(ConnectSocket);
-    WSACleanup();
-
-    return 0;
 }
+
