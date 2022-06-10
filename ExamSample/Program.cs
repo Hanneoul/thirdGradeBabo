d0125b91fbea7b72154c1fd9669c9a233362cd7b
PC2010221\YUHAN <e>
2022-06-11 01:10:02 +0900

    .

--- ExamSample/Program.cs
+++ ExamSample/Program.cs
@@ -1,46 +1,34 @@
 ﻿using System.Net;
-//using System.Collections.Generic;
-
-List<string> prefixes = new List<string>();
-
-//
+using System.Text.Json;
 
 
-
-if (!HttpListener.IsSupported)
-{
-    Console.WriteLine("Windows XP SP2 or Server 2003 is required to use the HttpListener class.");
-    return;
-}
-// URI prefixes are required,
-// for example "http://contoso.com:8080/index/".
-
-prefixes.Add("http://127.0.0.1:8080/index/");
-
-if (prefixes == null || prefixes.Count == 0)
-    throw new ArgumentException("prefixes");
-
-// Create a listener.
 HttpListener listener = new HttpListener();
-// Add the prefixes.
-foreach (string s in prefixes)
-{
-    listener.Prefixes.Add(s);
-}
+listener.Prefixes.Add("http://127.0.0.1:3000/GodDamn/");
 listener.Start();
 Console.WriteLine("Listening...");
-// Note: The GetContext method blocks while waiting for a request.
 HttpListenerContext context = listener.GetContext();
 HttpListenerRequest request = context.Request;
-// Obtain a response object.
 HttpListenerResponse response = context.Response;
-// Construct a response.
-string responseString = "<HTML><BODY> Hello world!</BODY></HTML>";
+response.AddHeader("Access-Control-Allow-Origin", "*");
+Stream body = request.InputStream;
+System.Text.Encoding encoding = request.ContentEncoding;
+System.IO.StreamReader reader = new System.IO.StreamReader(body, encoding);
+
+string rawurl = request.RawUrl;
+string httpmethod = request.HttpMethod;
+
+string result = "";
+
+result += string.Format("httpmethod = {0}\r\n", httpmethod);
+result += string.Format("rawurl = {0}\r\n", rawurl);
+System.Console.WriteLine(result);
+
+soo s = new soo();
+s.body = "MaryJane";
+
+string responseString = JsonSerializer.Serialize(s);
+System.Console.WriteLine(responseString);
 byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
-// Get a response stream and write the response to it.
-response.ContentLength64 = buffer.Length;
-System.IO.Stream output = response.OutputStream;
-output.Write(buffer, 0, buffer.Length);
-// You must close the output stream.
-output.Close();
+response.OutputStream.Write(buffer, 0, buffer.Length);
+response.OutputStream.Close();
 listener.Stop();
